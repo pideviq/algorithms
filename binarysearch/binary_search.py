70f6fe351fe889bedf8f8d32e0d9cad088d48482
Igor Puchnin <pideviq@gmail.com>
2022-05-10 19:58:22 +0300

    Add recursive version of Binary Search algorithm

--- binarysearch/binary_search.py
+++ binarysearch/binary_search.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
+from typing import List
 
-# NOTE: for Python 3.9 import is not required
+# NOTE: for Python 3.9 import of annotations is not required
 __version__ = '3.8'
 
 
-def binary_search(values: list, search) -> int | None:
+def binary_search(values: List[int], search) -> int | None:
     """Search an element in an array using Binary Search algorithm."""
     low = 0
     high = len(values) - 1
@@ -18,3 +19,25 @@ def binary_search(values: list, search) -> int | None:
         else:
             high = middle - 1
     return None
+
+
+def binary_search_recursive(values: List[int], search) -> int | None:
+    """Search an element in an array using Binary Search algorithm.
+
+    Divide-and-Conquer Edition.
+    """
+    # Base case
+    if not values:
+        return None
+    # Recursive case
+    high = len(values) - 1
+    middle = high // 2
+    guess = values[middle]
+    if guess == search:
+        return middle
+    elif guess > search:
+        return binary_search_recursive(values[:middle], search)
+    else:
+        new_low = middle + 1
+        index = binary_search_recursive(values[new_low:], search)
+        return None if index is None else new_low + index
--- binarysearch/test_binary_search.py
+++ binarysearch/test_binary_search.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 import os
-from binarysearch.binary_search import binary_search
+from binarysearch.binary_search import binary_search, binary_search_recursive
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -9,7 +9,8 @@ class TestBinarySearch(unittest.TestCase):
 
     def setUp(self) -> None:
         """Load test data."""
-        data_file_full_path = os.path.join(os.path.dirname(__file__), 'data.json')
+        data_file_full_path = os.path.join(os.path.dirname(__file__),
+                                           'data.json')
         with open(data_file_full_path) as test_cases_file:
             self.test_cases = json.load(test_cases_file)
         return None
@@ -19,19 +20,24 @@ class TestBinarySearch(unittest.TestCase):
         for case in self.test_cases.values():
             with self.subTest(case=case):
                 values, search = case['values'], case['search']
-                self.assertEqual(binary_search(values, search), values.index(search))
+                answer = values.index(search)
+                self.assertEqual(binary_search(values[:], search), answer)
+                self.assertEqual(binary_search_recursive(values[:], search),
+                                 answer)
 
     def test_none(self):
         """Test value out of list."""
         values = [3, 7, 12]
         search = 1
         self.assertIsNone(binary_search(values, search))
+        self.assertIsNone(binary_search_recursive(values, search))
 
     def test_empty_list(self):
         """Test empty list on input."""
         values = []
         search = 1
         self.assertIsNone(binary_search(values, search))
+        self.assertIsNone(binary_search_recursive(values, search))
 
 
 if __name__ == '__main__':
