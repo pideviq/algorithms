9dab19cdf43288dbe488ffa664b56f5ec64354cf
Igor Puchnin <pideviq@gmail.com>
2022-05-18 18:56:33 +0300

    Add Dijkstra's algorithm implementation

---
+++ graph/dijkstra.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+
+class Dijkstra:
+    """Dijkstra's algorithm implementation."""
+
+    def __init__(self, graph: dict) -> None:
+        self.graph = graph
+        self.costs = {}
+        self.parents = {}
+        self.processed = []
+        self.cheapest_path = None
+        infinity = float('inf')
+        # Set initial costs of edges of a graph and parents of nodes
+        for node in self.graph:
+            if node == 'start':
+                # Skip start node
+                continue
+            self.costs[node] = infinity
+            self.parents[node] = None
+        # Set cost and parent of start neighbors
+        for node, weight in self.graph['start'].items():
+            self.costs[node] = weight
+            self.parents[node] = 'start'
+
+    def calc_cheapest_path(self) -> int:
+        """Calculate the cheapest path from start to finish."""
+        if self.cheapest_path is None:
+            while (node := self._get_cheapest_node()) is not None:
+                for neighbor, neighbor_cost in self.graph[node].items():
+                    # Catch negative-weight edge
+                    if neighbor_cost < 0:
+                        raise ValueError('negative-weight edge detected.'
+                                         'Use Bellman-Ford algorithm instead')
+                    new_cost = self.costs[node] + neighbor_cost
+                    if new_cost < self.costs[neighbor]:
+                        self.costs[neighbor] = new_cost
+                        self.parents[neighbor] = node
+                self.processed.append(node)
+            self.cheapest_path = self.costs['finish']
+        return self.cheapest_path
+
+    def _get_cheapest_node(self) -> str | None:
+        """Get unprocessed node with the lowest cost.
+
+        Return None if no node found.
+        """
+        lowest_cost = float('inf')
+        lowest_cost_node = None
+        for node, cost in self.costs.items():
+            if node not in self.processed and cost < lowest_cost:
+                lowest_cost = cost
+                lowest_cost_node = node
+        return lowest_cost_node
---
+++ graph/test_dijkstra.py
@@ -0,0 +1,129 @@
+import unittest
+from graph.dijkstra import Dijkstra
+
+
+class TestDijkstra(unittest.TestCase):
+    """Test implementation of Dijkstra's algorithm."""
+
+    def setUp(self) -> None:
+        # Input
+        self.graph = {
+            'start': {
+                'a': 5,
+                'c': 2,
+            },
+            'a': {
+                'b': 4,
+                'd': 2,
+            },
+            'b': {
+                'd': 6,
+                'finish': 3,
+            },
+            'c': {
+                'a': 8,
+                'd': 7,
+            },
+            'd': {
+                'finish': 1,
+            },
+            'finish': {},
+        }
+        # Initial calculations
+        infinity = float('inf')
+        self.initial_costs = {
+            'a': 5,
+            'b': infinity,
+            'c': 2,
+            'd': infinity,
+            'finish': infinity,
+        }
+        self.initial_parents = {
+            'a': 'start',
+            'b': None,
+            'c': 'start',
+            'd': None,
+            'finish': None,
+        }
+        # Results
+        self.cheapest_path = 8
+        self.final_costs = {
+            'a': 5,
+            'b': 9,
+            'c': 2,
+            'd': 7,
+            'finish': 8,
+        }
+        self.final_parents = {
+            'a': 'start',
+            'b': 'a',
+            'c': 'start',
+            'd': 'a',
+            'finish': 'd',
+        }
+
+    def test_init(self):
+        """Test init functionality."""
+        dijkstra = Dijkstra(self.graph)
+        self.assertEqual(dijkstra.graph, self.graph)
+        self.assertEqual(dijkstra.costs, self.initial_costs)
+        self.assertEqual(dijkstra.parents, self.initial_parents)
+        self.assertIsNone(dijkstra.cheapest_path)
+
+    def test_calc_cheapest(self):
+        """Test calculation of the cheapest path."""
+        dijkstra = Dijkstra(self.graph)
+        self.assertEqual(dijkstra.calc_cheapest_path(), self.cheapest_path)
+        self.assertEqual(dijkstra.cheapest_path, self.cheapest_path)
+        self.assertEqual(dijkstra.costs, self.final_costs)
+        self.assertEqual(dijkstra.parents, self.final_parents)
+
+    def test_graph_with_loop(self):
+        """Test graph with loop."""
+        graph = {
+            'start': {
+                'a': 10,
+            },
+            'a': {
+                'b': 20,
+            },
+            'b': {
+                'c': 1,
+                'finish': 30,
+            },
+            'c': {
+                'a': 1,
+            },
+            'finish': {},
+        }
+        dijkstra = Dijkstra(graph)
+        dijkstra.calc_cheapest_path()
+        self.assertEqual(dijkstra.cheapest_path, 60)
+
+    def test_graph_with_negative_edge(self):
+        """Test graph with negative edge."""
+        graph = {
+            'start': {
+                'a': 2,
+                'b': 2,
+            },
+            'a': {
+                'c': 2,
+                'finish': 2,
+            },
+            'b': {
+                'a': 2,
+            },
+            'c': {
+                'b': -1,
+                'finish': 2,
+            },
+            'finish': {},
+        }
+        dijkstra = Dijkstra(graph)
+        with self.assertRaises(ValueError):
+            dijkstra.calc_cheapest_path()
+
+
+if __name__ == '__main__':
+    unittest.main()
